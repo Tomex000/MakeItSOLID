9cea24cb1b6b7e8c56dc94070319b07a26f8476d
Tomasz Krzyzak <tokrzyzak@gmail.com>
2021-06-18 00:43:35 +0200

    Task beginning

---
+++ FileGenerator/OldFileGenerator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileGenerator
+{
+    enum EFileFormat
+    {
+        other,
+        tiff,
+        jpg,
+    }
+
+    class OldFileGenerator
+    {
+        public bool ValidateFile(string path)
+        {
+            //to do
+        }
+        bool ValidateTIFF(string path)
+        {
+            return true;
+        }
+
+        bool ValidateJPG(string path)
+        {
+            return true;
+        }
+        EFileFormat GetFormat(string path)
+        {
+            return EFileFormat.jpg;
+        }
+    }
+}
